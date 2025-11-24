7840352918b26393495324a780e8bb3ac799bcf2
BrianBut <brianjbutcher65@gmail.com>
2025-11-24 07:17:22 +0000

    modified:   todos.py Deletion done

--- todos.py
+++ todos.py
@@ -10,39 +10,47 @@ def todos(session):
     assert(session.get('auth') == 'Admin')
     nav_items=["Home"]
     # List only shows not dones ordered by date notified
-    todo_links= [ Grid(A(todo.title, todo.description, todo.comments, href='/todos/edit_todo/{}'.format(todo.id)),
+    todo_links= [ Grid(A(todo.title, todo.comments, href='/todos/edit_todo/{}'.format(todo.id)),
         A('Mark Done', href='/todos/mark_done/{}'.format(todo.id), style='text-align: right'),
         style='text-align: left'
         ) for todo in Todo.select().order_by(Todo.notified).where(Todo.done == datetime.min)]
+    done_links= [ Grid(A(todo.title, todo.comments, href='/todos/edit_todo/{}'.format(todo.id)),
+        A('Mark Not Done', href='/todos/mark_not_done/{}'.format(todo.id), style='text-align: right'),
+        A('Delete', href='/todos/delete/{}'.format(todo.id), style='text-align: right'),
+        style='text-align: left'
+        ) for todo in Todo.select().order_by(Todo.notified).where(Todo.done > datetime.min)]
     return Container(
         common_header(nav_items,'Todos', session),
         Hr(),
         Ul(*todo_links),
-        A(Button('New Todo'), href='/todos/new_todo'), style='text-align: right')
-        # Toggle completed todos
+        A(Button('New Todo'), href='/todos/new_todo', style='text-align: right'),
+        Hr(),
+        Ul(*done_links),
+        )
+
 
 @rt
 def new_todo(session):
     assert(session.get('auth') == 'Admin')
     frm = Form(action=send_todo, method='post')(
         Input(type="text", name="title", placeholder="Title"),
-        Textarea(name="description", placeholder="Description", rows=5),
-        Textarea(name="comments", placeholder="comments", rows=2),
+        Textarea(name="comments", placeholder="comments", rows=5),
         Hidden(name="owner", value=session),
         Button("Create New Todo"))
     return (Titled('New Todo', frm))
 
 @rt
-def send_todo(session, title:str, description:str, comments:str):
+def send_todo(session, title:str, comments:str):
     username= session.get('auth')
     user= User.get(User.name==session.get('auth'))
     logging.info("UserId is {}".format(user.id))
-    logging.info("in send_new_todo: title is '{}', description is '{}', comments are '{}', owner is {}".format(title, description, comments, user.id ))
-    try: #New Todo
+    logging.info("in send_new_todo: title is '{}', comments are '{}', owner is {}".format(title, comments, user.id ))
+    try:
         todo= Todo.get(title=title)
-    except: #Editing
-        todo= Todo( title=title, description=description, comments=comments, owner_id=user.id, last_edited=datetime.utcnow())
+    except:
+        todo= Todo( title=title, comments=comments, owner_id=user.id, last_edited=datetime.utcnow())
     logging.info("todo.title is '{}'".format(todo.title))
+    todo.comments= comments
     todo.save()
     logging.info("todo should be saved and redirected to {}".format('/todos/'))
     return RedirectResponse('/todos/', status_code=303)
@@ -53,12 +61,10 @@ def edit_todo(id:int):
     logging.info("in edit_todo/{}".format(id))
     todo = Todo.get(id=id)
     logging.info("todo title is {}".format(todo.title))
-    logging.info("todo description is {}".format(todo.description))
     logging.info("todo comments are {}".format(todo.comments))
     frm = Form(action=send_todo, method='post')(
         Hidden(name="title", value=todo.title),
         # Note the way textareas are set
-        Textarea( todo.description, id='description', rows=5),
         Textarea( todo.comments, id='comments', rows=5),
         Button("Submit")
     )
@@ -72,4 +78,20 @@ def get(id:int):
     todo.done= datetime.utcnow()
     todo.save()
     return RedirectResponse('/todos')
+
+
+@rt('/mark_not_done/{id}')
+def get(id:int):
+    todo = Todo.get(id)
+    todo.done= datetime.min
+    todo.save()
+    return RedirectResponse('/todos')
+
+@rt('/delete/{id}')
+def get(id:int):
+    todo = Todo.get(id)
+    todo.delete_instance()
+    return RedirectResponse('/todos')
+
+
 
